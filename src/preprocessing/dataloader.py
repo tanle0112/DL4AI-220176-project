5780739651af7cfdef933aa8b1695e6ebdc01618
tanle0112 <tan.le.220176@student.fulbright.edu.vn>
2026-03-04 22:00:14 +0700

    task 1.1: use multiple features rather than only 'Open'
    - load AAPL.csv
    - read data + multi features

--- src/preprocessing/dataloader.py
+++ src/preprocessing/dataloader.py
@@ -2,5 +2,35 @@
 #chia train, val, test 
 #normalize 0,1 
 #resize (if need) 
-def get_dataloader():
-     return train_loader, val_loader, test_loader
+#def get_dataloader():
+#    return train_loader, val_loader, test_loader
+
+#Task 1.1 use multi features rather than just only Open 
+     #load data AAPL.csv 
+import pandas as pd 
+import os 
+
+def load_raw_data():
+     base_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+     file_path = os.path.join(base_path, "data", "AAPL.csv")
+
+     df = pd.read_csv(file_path)
+     return df
+
+
+if __name__ == "__main__":
+    df = load_raw_data()
+    print(df.head())
+    print(df.columns)
+    
+    #multi features 
+from sklearn.preprocessing import MinMaxScaler 
+import numpy as np 
+
+def load_and_preprocessing(time_step=60):
+     df = load_raw_data()
+     features = ['Low', 'High', 'Open', 'Close', 'Adjusted Close', 'Volume']
+     data = df[features].values
+     scaler = MinMaxScaler()
+     data_scaled = scaler.fit_transform(data)
+     return data_scaled, scaler 
